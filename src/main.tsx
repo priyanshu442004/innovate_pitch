b23d4b82a086ea8ea6ce1d35d9d11f46ea3b135c
priyanshu442004 <145048420+priyanshu442004@users.noreply.github.com>
2025-11-19 14:07:50 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+  <GoogleOAuthProvider clientId={"251993202162-mutfkig1lvhgivu126bdfl4iqhshjmbk.apps.googleusercontent.com"}>
+  <App />
+</GoogleOAuthProvider>
+);
   
